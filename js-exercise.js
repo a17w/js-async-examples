7e661c073551ef41d2d444b1afb2745229440b58
anniehswong <annie.hs.wong@gmail.com>
2017-03-04 17:54:08 -0800

    ruby sync vs. js async

---
+++ js-exercise.js
@@ -0,0 +1,41 @@
+function firstTask() {
+  console.log("first task complete");
+}
+
+function secondTask() {
+  console.log("second task complete");
+}
+
+function thirdTask() {
+  console.log("third task complete");
+}
+
+setTimeout(firstTask, 2000);
+secondTask();
+thirdTask();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js-exercise.rb
@@ -0,0 +1,25 @@
+def first_task
+  puts "first task complete"
+end
+
+def second_task
+  puts "second task complete"
+end 
+
+def third_task
+  puts "third_task"
+
+sleep 5 
+first_task 
+second_task
+third_task
+
+
+
+
+
+
+
+
+
+
